bfbbfd3fe1bb552fabf31c863eee1bc8945872db
JavaHammes <alexander.berlin13@gmail.com>
2022-01-23 10:59:19 +0100

    first

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "index.html",
+        name = name
+    )
